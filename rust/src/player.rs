87d875d867a7ae9855ad23bb1c482ef362b8b626
Patrick K. Byers <pbyers413@gmail.com>
2025-04-21 22:59:58 -0400

    ready to test hello world?

---
+++ rust/src/player.rs
@@ -0,0 +1,25 @@
+use godot::prelude::*;
+use godot::classes::Sprite2D;
+
+#[derive(GodotClass)]
+#[class(base=Sprite2D)]
+struct Player {
+    speed: f64,
+    angular_speed: f64,
+
+    base: Base<Sprite2D>
+}
+use godot::classes::ISprite2D;
+
+#[godot_api]
+impl ISprite2D for Player {
+    fn init(base: Base<Sprite2D>) -> Self {
+        godot_print!("Hello, world!"); // Prints to the Godot console
+        
+        Self {
+            speed: 400.0,
+            angular_speed: std::f64::consts::PI,
+            base,
+        }
+    }
+}
